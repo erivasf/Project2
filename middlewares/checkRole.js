78cc045984833ce618c3cd0876db92b122635802
erivasf <elias_rf@icloud.com>
2019-09-20 10:09:19 -0500

    Minor changes

---
+++ middlewares/checkRole.js
@@ -0,0 +1,9 @@
+const User = require()
+module.exports = route => (req, res, next) => {
+  const user = req.user
+  if (user.space.length!= 0) {
+    next()
+  } else {
+    res.redirect(route)
+  }
+}
